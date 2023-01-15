1a259b907e8614a96a2cb4f2969d4f5df9ea8bd9
Igor <traver.unimmer@gmail.com>
2023-01-15 18:32:30 +0300

    feat: complete task training-js-10

---
+++ training-js/10/script.js
@@ -0,0 +1,9 @@
+function pickIt(arr){
+    var odd=[],even=[];
+    //coding here
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] % 2 === 0 ? even.push(arr[i]) : odd.push(arr[i]);
+    }
+    
+    return [odd,even];
+  }

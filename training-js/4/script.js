222b79a8bbe59c8bd0962c2aae10f193b3140608
Igor <traver.unimmer@gmail.com>
2023-01-15 17:43:45 +0300

    feat: complete task training-js-4

--- training-js/4/script.js
+++ training-js/4/script.js
@@ -1,28 +1,23 @@
-var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
-function Dad(){
-  //select some variable to combine "Dad"
-  return d1+a2+d2;
+function getLength(arr){
+  //return length of arr
+  return arr.length;
 }
-function Bee(){
-  //select some variable to combine "Bee"
-  return b1+e2+e2;
+function getFirst(arr){
+  //return the first element of arr
+  return arr[0]
 }
-function banana(){
-  //select some variable to combine "banana"
-  return b2+a2+n2+a2+n2+a2;
+function getLast(arr){
+  //return the last element of arr
+  return arr[arr.length - 1];
 }
-
-//answer some questions if you finished works above
-function answer1(){
-  //the answer should be "yes" or "no"
-  return "no";
+function pushElement(arr){
+  var el=1;
+  //push el to arr
+  arr.push(el);
+  return arr
 }
-function answer2(){
-  //the answer should be "yes" or "no"
-  return "no";
+function popElement(arr){
+  //pop an element from arr
+  arr.pop()
+  return arr;
 }
-function answer3(){
-  //the answer should be "yes" or "no"
-  return "yes";
-}
-
